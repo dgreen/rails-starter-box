2082d2d4ea95d0a38d84125e124221e22a220290
David Green <d.green@ieee.org>
2013-07-19 13:13:53 -0500

    Modified Vagrant file re ports, dependencies, install passenger
    Installing passenger, installs apache which starts automatically
    Created variables for ruby versions to ease maintenance

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,13 @@ Vagrant::Config.run do |config|
   config.vm.box_url   = 'http://files.vagrantup.com/precise32.box'
   config.vm.host_name = 'rails-starter-box'
 
+# 3000 - rails server
+# 8888 - cas server
+# 8080 - outside view of Apache Web Server
+
   config.vm.forward_port 3000, 3000
+  config.vm.forward_port 8888, 8888
+  config.vm.forward_port 80, 8080
 
   config.vm.provision :puppet,
     :manifests_path => 'puppet/manifests',
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,3 +1,9 @@
+
+
+# create some variables for ease of updating
+$main_ruby_version = 'ruby-1.9.3-p448'
+$legacy_ruby_version = 'ruby-1.8.7-p370'
+
 # Make sure apt-get -y update runs before anything else.
 stage { 'preinstall':
   before => Stage['main']
@@ -43,22 +49,22 @@ class install_mysql {
 }
 class { 'install_mysql': }
 
-class install_postgres {
-  class { 'postgresql': }
+# class install_postgres {
+#   class { 'postgresql': }
 
-  class { 'postgresql::server': }
+#   class { 'postgresql::server': }
 
-  pg_user { 'vagrant':
-    ensure    => present,
-    superuser => true,
-    require   => Class['postgresql::server']
-  }
+#   pg_user { 'vagrant':
+#     ensure    => present,
+#     superuser => true,
+#     require   => Class['postgresql::server']
+#   }
 
-  package { 'libpq-dev':
-    ensure => installed
-  }
-}
-class { 'install_postgres': }
+#   package { 'libpq-dev':
+#     ensure => installed
+#   }
+# }
+# class { 'install_postgres': }
 
 
 
@@ -67,23 +73,40 @@ class install-rvm {
   rvm::system_user { vagrant: ; }
 
   rvm_system_ruby {
-    'ruby-1.9.3-p194':
+    "$main_ruby_version":
       ensure => 'present',
       default_use => false;
-    'ruby-1.8.7-p370':
+    "$legacy_ruby_version":
       ensure => 'present',
       default_use => false;
   }
 
+  # Install the gems after the corresponding ruby is installed.
+
   rvm_gem {
-    'ruby-1.9.3-p194/bundler': ensure => latest;
-    'ruby-1.9.3-p194/rails': ensure => latest;
-    'ruby-1.9.3-p194/rake': ensure => latest;
-    'ruby-1.8.7-p370/bundler': ensure => latest;
-    'ruby-1.8.7-p370/rails': ensure => latest;
-    'ruby-1.8.7-p370/rake': ensure => latest;
+    'ruby-1.9.3-p448/bundler': ensure => '1.3.5', require => Rvm_system_ruby["$main_ruby_version"];
+    'ruby-1.9.3-p448/rails': ensure => '3.2.13', require => Rvm_system_ruby["$main_ruby_version"];
+    'ruby-1.9.3-p448/rake': ensure => '10.1.0', require => Rvm_system_ruby["$main_ruby_version"];
+#   'ruby-1.8.7-p370/bundler': ensure => latest, require => Rvm_system_ruby["$legacy_ruby_version"];
+#   'ruby-1.8.7-p370/rails': ensure => latest, require => Rvm_system_ruby["$legacy_ruby_version"];;
+#   'ruby-1.8.7-p370/rake': ensure => latest, require => Rvm_system_ruby["$legacy_ruby_version"];;
   }
 
 }
 
 class { 'install-rvm': }
+
+# from: https://github.com/blt04/puppet-rvm
+# not yet being executed
+
+# Define and install a passenger install (probably needs work)
+
+class {
+  'rvm::passenger::apache':
+    version => '3.0.11',
+    ruby_version => "$main_ruby_version",
+    mininstances => '3',
+    maxinstancesperapp => '0',
+    maxpoolsize => '30',
+    spawnmethod => 'smart-lv2';
+}
